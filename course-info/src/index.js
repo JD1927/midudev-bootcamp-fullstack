01b014bb60840c0b63dfa4817c41078bc22babec
Juan Aguirre <j1927aguirre@gmail.com>
2022-12-21 13:00:40 -0500

    Add TitleComponent

--- course-info/src/index.js
+++ course-info/src/index.js
@@ -12,7 +12,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
+      <Title course={course} />
       <p>
         {part1} {exercises1}
       </p>
@@ -25,6 +25,10 @@ const App = () => {
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
+};
+
+const Title = ({ course }) => {
+  return <h1>{course}</h1>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
